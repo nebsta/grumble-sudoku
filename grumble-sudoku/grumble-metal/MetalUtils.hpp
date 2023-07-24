27d166435abb9a81028016d6d5f573c44b46e2eb
Benjamin Wallis <bewa@trustpilot.com>
2023-07-24 13:50:42 +0200

    fixing up translation matrix

--- grumble-sudoku/grumble-metal/MetalUtils.hpp
+++ grumble-sudoku/grumble-metal/MetalUtils.hpp
@@ -10,6 +10,7 @@
 #include <Metal/Metal.hpp>
 #include <simd/simd.h>
 #include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 #include <grumble/render/RenderMethod.hpp>
 
@@ -23,18 +24,19 @@ public:
   }
   
   static simd::float4x4 to_simd_float4x4(glm::mat4 matrix) {
-    return simd::float4x4(simd::make_float4(matrix[0][0], matrix[1][0], matrix[2][0], matrix[3][0]),
-                          simd::make_float4(matrix[0][1], matrix[1][1], matrix[2][1], matrix[3][1]),
-                          simd::make_float4(matrix[0][2], matrix[1][2], matrix[2][2], matrix[3][2]),
-                          simd::make_float4(matrix[0][3], matrix[1][3], matrix[2][3], matrix[3][3]));
+    grumble::Logger::info(glm::to_string(matrix));
+    return simd::float4x4(simd::make_float4(matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3]),
+                          simd::make_float4(matrix[1][0], matrix[1][1], matrix[1][2], matrix[1][3]),
+                          simd::make_float4(matrix[2][0], matrix[2][1], matrix[2][2], matrix[2][3]),
+                          simd::make_float4(matrix[3][0], matrix[3][1], matrix[3][2], matrix[3][3]));
   }
   
   
   static simd_float4x4 makeTranslationMatrix(simd_float1 tx, simd_float1 ty) {
     simd_float4x4 matrix = matrix_identity_float4x4;
     
-    matrix.columns[3][0] = tx;
-    matrix.columns[3][1] = ty;
+    matrix.columns[3].xy = simd_make_float2(tx, ty);
+//    matrix.columns[3] = ty;
     
     return matrix;
   }
